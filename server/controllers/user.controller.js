26ef5911cf12f88608e001a5d39e9160c4bca0e2
Santiago Martínez <xsantysxx@gmail.com>
2020-07-03 20:44:18 -0500

    Cambio para que el toquen expire cada 10 minutos

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -44,7 +44,7 @@ usersCtrl.signin = async (req, res) => {
   })
   .then((validation)=>{
     if(validation){
-      jwt.sign({ user:email },'colapp',{expiresIn: 60},(err,token)=>{
+      jwt.sign({ user:email },'colapp',{expiresIn: 60*10},(err,token)=>{
         res.json({
           success: true,
           status: "Ok, logged",
